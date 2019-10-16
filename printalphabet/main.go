33bfaf43a1a99ab79a97dc6fa61606abe6faef36
IamhappyXD <50207701+IamhappyXD@users.noreply.github.com>
2019-10-16 21:37:53 +0600

    Update main.go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,8 +4,8 @@ func main(){
 	
 	var aRune string ="abcdefghijklmnopqrstuvwxyz"
 	for i:= 0;i<26;i++{
-	z01.PrintRune(rune(aRune[i]))
+		z01.PrintRune(rune(aRune[i]))
 	}
-	var bRune rune= '\n'
+	var bRune rune = '\n'
 	z01.PrintRune(bRune)
 }
