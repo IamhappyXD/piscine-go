b07b3a8e28c4ead84e8560fbd7d7f941ba4188ee
IamhappyXD <50207701+IamhappyXD@users.noreply.github.com>
2019-10-16 21:54:02 +0600

    Add files via upload

---
+++ printreversealphabet/main.go
@@ -0,0 +1,13 @@
+
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	var aRune string = "abcdefghijklmnopqrstuvwxyz"
+	for i := 25; i >= 0; i-- {
+		z01.PrintRune(rune(aRune[i]))
+	}
+	var bRune rune = '\n'
+	z01.PrintRune(bRune)
+}
