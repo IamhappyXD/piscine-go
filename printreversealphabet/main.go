2364ff522258750c1da4fa61a0552afc64d1ec99
iamhappyXD <aminashaikym@gmail.com>
2019-10-16 23:32:01 +0600

    ghjlk63

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -1,15 +1,11 @@
 package main
 
-import "fmt"
 import "github.com/01-edu/z01"
+import "fmt"
 
 func main() {
-<<<<<<< HEAD
 	for i := 122; i > 96; i-- {
 
-=======
-	for i := 122; i > 96; i-- {	 
->>>>>>> 6db51231c85808830b7de24f94a6d0a108132041
 		ch := string(i)
 		fmt.Printf(ch)
 	}
