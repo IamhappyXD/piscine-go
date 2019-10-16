a6eaba9129781a496dbe30f94f3debaa44710f71
iamhappyXD <aminashaikym@gmail.com>
2019-10-16 22:49:50 +0600

    llalalal

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,9 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var aRune string = "abcdefghijklmnopqrstuvwxyz"
+	var xRune string = "abcdefghijklmnopqrstuvwxyz"
 	for i := 25; i >= 0; i-- {
-		z01.PrintRune(rune(aRune[i]))
+		z01.PrintRune(rune(xRune[i]))
 	}
 	z01.PrintRune('\n')
 }
