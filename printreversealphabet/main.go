93a9991207404b2ce54b808363eb69eed94c35e7
iamhappyXD <aminashaikym@gmail.com>
2019-10-16 23:27:14 +0600

    ghjlk63

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -1,11 +1,13 @@
 package main
 
+import "fmt"
 import "github.com/01-edu/z01"
 
 func main() {
-	var xRune string = "abcdefghijklmnopqrstuvwxyz"
-	for i := 25; i >= 0; i-- {
-		z01.PrintRune(rune(xRune[i]))
+	for i := 122; i > 96; i-- {
+
+		ch := string(i)
+		fmt.Printf(ch)
 	}
 	z01.PrintRune(10)
 }
