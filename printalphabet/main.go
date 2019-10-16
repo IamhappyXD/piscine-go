39bb97ffd5de8eadfefde8ccc524cfec13a24551
IamhappyXD <50207701+IamhappyXD@users.noreply.github.com>
2019-10-16 21:40:07 +0600

    Update main.go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,9 +1,11 @@
 package main
+
 import "github.com/01-edu/z01"
+
 func main() {
 	
 	var aRune string = "abcdefghijklmnopqrstuvwxyz"
-	for i := 0; i <26; i++ {
+	for i := 0; i < 26; i++ {
 		z01.PrintRune(rune(aRune[i]))
 	}
 	var bRune rune = '\n'
