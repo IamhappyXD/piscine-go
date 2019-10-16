e3aa06bed5275efd4229508df191ae8755d6243e
IamhappyXD <50207701+IamhappyXD@users.noreply.github.com>
2019-10-16 23:31:24 +0600

    Update main.go

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -4,12 +4,7 @@ import "fmt"
 import "github.com/01-edu/z01"
 
 func main() {
-<<<<<<< HEAD
-	for i := 122; i > 96; i-- {
-
-=======
 	for i := 122; i > 96; i-- {	 
->>>>>>> 6db51231c85808830b7de24f94a6d0a108132041
 		ch := string(i)
 		fmt.Printf(ch)
 	}
