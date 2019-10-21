e4553461551d0827037a3c848779ebde79d04320
iamhappyXD <aminashaikym@gmail.com>
2019-10-21 21:42:34 +0600

    jhgj

---
+++ sqrt.go
@@ -0,0 +1,23 @@
+package piscine
+
+func Sqrt(nb int) int {
+	if nb < 0 {
+		return 0
+	} else if nb == 0 || nb == 1 {
+		return nb
+	} else {
+		result := 1
+		i := 1
+		for result <= nb {
+			result = i * i
+			i++
+		}
+		if i*i == nb {
+			return i - 1
+		} else {
+			return i - 2
+		}
+
+	}
+
+}
