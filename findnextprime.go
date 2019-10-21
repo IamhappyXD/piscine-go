3ed013432a9cbb4628ab2d08d969902aea4d3cee
iamhappyXD <aminashaikym@gmail.com>
2019-10-21 23:30:48 +0600

    ujh

--- findnextprime.go
+++ findnextprime.go
@@ -1,30 +1,33 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if nb <= 1 {
+	if nb <= 1 || nb == 2 {
 		return 2
 	} else {
-		for i := nb; i < nb+20; i++ {
-			check := 0
-			if i/10 > 1 && (i%2 == 0 || i%10 == 2 || i%10 == 5 || i%10 == 0) {
+		i := nb
+
+		for i < nb+12 {
+
+			if (nb > 10) && ((i%2 == 0) || (i%10 == 2) || (i%10 == 5) || (i%10 == 0)) {
+				i++
 				continue
+			} else {
+				check := 0
+				for j := 2; j < i; j++ {
+					if i%j == 0 {
+						check++
+						break
+					}
+				}
+				if check == 0 {
+					return i
+				}
+				i++
 			}
 
-			for j := 2; j < i; j++ {
-				if i%j == 0 {
-					check++
-					break
-				}
-				if i%(j*j) == 0 {
-					check++
-					break
-				}
-			}
-			if check == 0 {
-				return i
-			}
 		}
 		return nb
+
 	}
 
 }
