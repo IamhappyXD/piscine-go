c2267ae36b9143bd6d5954d68b9a64fa47fb184b
IamhappyXD <50207701+IamhappyXD@users.noreply.github.com>
2019-10-21 14:30:06 +0600

    Add files via upload

---
+++ iterativefactorial.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func IterativeFactorial(nb int) int {
+	result:=1
+	if nb == 0{
+		return result
+	}else{
+		for i: =1;i<=nb;i++{
+			result=result*i
+		}
+	}
+	return result
+		
+}
+func main() {
+	arg := 4
+	fmt.Println(piscine.IterativeFactorial(arg))
+}
