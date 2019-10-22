12afd538831ce37980ce3571f5c6c53c58b25ffd
iamhappyXD <aminashaikym@gmail.com>
2019-10-22 21:22:00 +0600

    454

--- trimatoi.go
+++ trimatoi.go
@@ -4,14 +4,17 @@ func TrimAtoi(s string) int {
 	var a []int
 	check := []rune(s)
 	count := 0
+	minus := 0
 	for i := range check {
 		if check[i] >= 48 && check[i] <= 57 {
 			a = append(a, int(check[i])-48)
 			count++
 		}
+		if check[i] == 45 {
+			minus++
+		}
 
 	}
-
 	if count == 0 {
 		return 0
 	} else {
@@ -21,8 +24,12 @@ func TrimAtoi(s string) int {
 			x += a[i]
 
 		}
-
-		return x
+		if minus == 0 {
+			return x
+		} else {
+			min := 0 - x
+			return min
+		}
 
 	}
 
