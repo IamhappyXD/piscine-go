a8bb5ef194c40fa04581d5673b62d5c9cac4374f
iamhappyXD <aminashaikym@gmail.com>
2019-10-22 22:24:40 +0600

    kkj

---
+++ index.go
@@ -0,0 +1,27 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	main := []rune(s)
+	check := []rune(toFind)
+	ind := 0
+	count := 0
+	total := 0
+
+	for i := range main {
+		for j := range check {
+			if main[i] == check[j] {
+				count++
+			} else {
+				break
+			}
+			total++
+
+		}
+
+		if total == count && total > 0 {
+			return ind
+		}
+		ind++
+	}
+	return -1
+}
