e836e4ec6491fdbf1f2b06d49c077cae08b24a41
iamhappyXD <aminashaikym@gmail.com>
2019-10-22 19:31:01 +0600

    ljl

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -1,37 +1,31 @@
-package piscine 
+package piscine
 
 func PrintNbrInOrder(n int) {
-	if n <= 0{
+	if n <= 0 {
 		fmt.Print(0)
-	}else if 1 <= n && n<=9 {
+	} else if 1 <= n && n <= 9 {
 		fmt.Print(n)
-	} else{
-		i :=0
-		ch :=n
-		var a[] int
-		for 1<ch{
-			a=append(a, ch%10)
-			ch -= (ch%10) 
+	} else {
+		i := 0
+		ch := n
+		var a []int
+		for 1 < ch {
+			a = append(a, ch%10)
+			ch -= (ch % 10)
 			ch /= 10
 			i++
 		}
-		for j:=0; j< i-1; j++{
-			if a[j] > a[j+1]{
+		for j := 0; j < i-1; j++ {
+			if a[j] > a[j+1] {
 				temp := a[j+1]
 				a[j] = a[j+1]
 				a[j+1] = temp
 			}
 		}
 
-		for j:=0; j< i;j++{
+		for j := 0; j < i; j++ {
 			fmt.Print(a[j])
 		}
 
-	
-		
 	}
 }
-
-
-
-
