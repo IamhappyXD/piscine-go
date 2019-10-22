e2cad00297ac2288cc91c6cc3f252be8e8ae7891
Amina Shaikym <aminashaikym@Aminas-MacBook-Pro.local>
2019-10-22 14:50:30 +0600

    last try

--- findnextprime.go
+++ findnextprime.go
@@ -1,38 +1,16 @@
-package piscine
+package main
 
 func FindNextPrime(nb int) int {
 	if nb <= 1 || nb == 2 {
 		return 2
 	} else {
-
-		i := nb
-
-		for i < nb+12 {
-			if (nb > 10) && ((i%2 == 0) || (i%10 == 2) || (i%10 == 5) || (i%10 == 0)) {
-				i++
-				continue
-			} else {
-				check := 0
-				for j := 2; j < i; j++ {
-					if i%j == 0 {
-						check++
-						break
-					}
-					if i%(j*j) == 0 {
-						check++
-						break
-					}
-				}
-
-				if check == 0 {
-					return i
-				}
-				i++
-
+		for j := 2; j < nb; j++ {
+			if nb%j == 0 {
+				return FindNextPrime(nb + 1)
 			}
-
 		}
 		return nb
 
 	}
+
 }
