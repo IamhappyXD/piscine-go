b7f45d00c756d1c4c1feb478393fc5f2033d52b8
iamhappyXD <aminashaikym@gmail.com>
2019-10-22 22:08:20 +0600

    kjhk

--- lastrune.go
+++ lastrune.go
@@ -2,12 +2,12 @@ package piscine
 
 func LastRune(s string) rune {
 	check := []rune(s)
-	count := 1
+	count := 0
 	for i := range check {
 		count++
 		if check[i] == 0 {
 			return 0
 		}
 	}
-	return check[count]
+	return check[count-1]
 }
