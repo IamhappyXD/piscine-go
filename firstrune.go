eb4d8f6d41a6bafccd3e5dfc467fea743e751972
iamhappyXD <aminashaikym@gmail.com>
2019-10-22 21:41:47 +0600

    ljk

--- firstrune.go
+++ firstrune.go
@@ -1,16 +1,6 @@
-package main
-
-import (
-	"github.com/01-edu/z01"
-)
+package piscine 
 
 func FirstRune(s string) rune {
 	check := []rune(s)
 	return check[0]
-}
-func main() {
-	z01.PrintRune(FirstRune("Hello!"))
-	z01.PrintRune(FirstRune("Salut!"))
-	z01.PrintRune(FirstRune("Ola!"))
-	z01.PrintRune('\n')
 }
