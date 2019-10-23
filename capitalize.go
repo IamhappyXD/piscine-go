6d9988156e26d457b7b450e03fd4875bcabbfb0d
iamhappyXD <aminashaikym@gmail.com>
2019-10-23 19:13:54 +0600

    ikjl

--- capitalize.go
+++ capitalize.go
@@ -1,28 +1,35 @@
-package piscine
+package main
+
+import "fmt"
 
 func Capitalize(s string) string {
 	check := []rune(s)
 	start := 0
 	for i := range check {
 		if start == 0 {
-			if 97 <= check[i] && check[i] <= 122 { //toupper
+			if 65 <= check[i] && check[i] <= 90 {
+				start++
+				continue
+			} else if 97 <= check[i] && check[i] <= 122 { //toupper
 				check[i] = check[i] - 32
 				start++
-			} else if 65 <= check[i] && check[i] <= 90 {
-				start++
-			} else {
 				continue
+			} else {
+				start++
 			}
-
-			continue
 		}
+
 		if 65 <= check[i] && check[i] <= 90 { //tolower
 			check[i] = check[i] + 32
-		} else if 97 <= check[i] && check[i] <= 122 {
+		} else if (97 <= check[i] && check[i] <= 122) || (check[i] >= 48 && check[i] <= 57) {
 			continue
 		} else {
 			start = 0
 		}
 	}
 	return string(check)
+}
+
+func main() {
+	fmt.Println(Capitalize("J*+4bGZX(dB}U"))
 }
