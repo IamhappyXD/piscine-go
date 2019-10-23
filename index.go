a6a64383b7b8832ad0266feee59388b554baf3ab
iamhappyXD <aminashaikym@gmail.com>
2019-10-23 17:46:25 +0600

    hjhn

--- index.go
+++ index.go
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
-package main
-import "fmt"
+package piscine
+
 func Index(s string, toFind string) int {
 	main := []rune(s)
 	check := []rune(toFind)
-	chlen :=0
-	mlen :=0
+	chlen := 0
+	mlen := 0
 	for j := range check {
 		if check[j] == 0 {
 			return 0
@@ -18,67 +17,33 @@ func Index(s string, toFind string) int {
 		}
 		mlen++
 	}
-	if chlen<= mlen{
-		j :=0
-		ind :=0
-		for i :=0;i<mlen;i++{
-			if(main[i]==check[j]){
-				j++
-			}else{
-				ind=i
-				j=0
-			}
-			if j == chlen{	
-				break
-			}
-		}
-		if(j == chlen){
-			return ind+1
-		}else{
-			return -1
-		}
-	
-	}else{
-		return -1
-	}
-
-}
-func  main(){
-	fmt.Println(Index("Amina", "Ami"))
-=======
-package piscine
-
-func Index(s string, toFind string) int {
-	main := []rune(s)
-	check := []rune(toFind)
-	ind := 0
-	count := 0
-	total := 0
-
-	for j := range check {
-		total++
-		if check[j] == 0 {
-			return 0
-		}
-	}
-
-	for i := range main {
-		for j := range check {
+	if chlen <= mlen {
+		j := 0
+		ind := 0
+		for i := 0; i < mlen+1; i++ {
 			if main[i] == check[j] {
-				count++
+				j++
+				if j == chlen {
+					break
+				}
+			} else {
+				ind = i
+				j = 0
 			}
 
 		}
+		if j == chlen {
+			if ind == 0 && main[0] == check[0] {
+				return 0
+			} else {
+				return ind + 1
+			}
 
-		if total == count {
-			return ind
 		} else {
-			ind++
-			count = 0
-			total = 0
+			return -1
 		}
 
 	}
 	return -1
->>>>>>> 0c77c8c142162b62eea3b070c6ec68f2d5801038
+
 }
