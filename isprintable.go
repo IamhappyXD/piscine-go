02adc1b7ecebca7431b398a0165fb781e91c5bd7
iamhappyXD <aminashaikym@gmail.com>
2019-10-23 19:53:19 +0600

    kjkj

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(str string) bool {
 	check := []rune(str)
 	for i := range check {
-		if 33 <= check[i] && check[i] <= 126 {
+		if 32 <= check[i] && check[i] <= 126 {
 			continue
 		} else {
 			return false
