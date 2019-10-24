818b908267d03e6c62c3551e50c212a2fc98aeb2
iamhappyXD <aminashaikym@gmail.com>
2019-10-24 20:01:01 +0600

    kjhk

---
+++ sortparams/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+
+	count := 0
+	for range os.Args {
+		count++
+	}
+
+	for i :=0;i<count-1;i++{
+		for j:=i+1;j<count-1;j++{
+			if os.Args[i]>os.Args[j]{
+				temp := os.Args[j]
+				os.Args[j]=os.Args[i]
+				os.Args[i]=temp
+			}
+		}
+	}
+	i := 1
+	for i < count {
+		out := []rune(os.Args[i])
+		for _, j := range out {
+			z01.PrintRune(j)
+		}
+		z01.PrintRune(10)
+		i++
+	}
+	
+
+}
