92530e9ee693ad681a167e5aa236ebd7a58401b1
iamhappyXD <aminashaikym@gmail.com>
2019-10-25 20:56:05 +0600

    mdsds

--- index.go
+++ index.go
@@ -1,4 +1,5 @@
-package piscine
+package main
+import "fmt"
 
 func Index(s string, toFind string) int {
 	main := []rune(s)
@@ -13,15 +14,38 @@ func Index(s string, toFind string) int {
 			return 0
 		}
 	}
-
-	for i := range main {
-		for j := range check {
-			if main[i] == check[j] {
-				count++
-			}
-
+	for j := range main {
+		count++
+		if main[j] == 0 {
+			return 0
 		}
-		ind++
 	}
-	return -1
+	if total>count{
+		return -1
+	}else{
+		equal :=0
+		for i := range main {
+			for j :=i; j<total; j++ {
+				if check[j] == main[i] {
+					equal++
+					
+				}
+	
+			}
+			fmt.Println(equal)
+			if equal ==total{
+				return ind
+			}
+			equal =0
+			ind++
+		}
+		return -1
+	}
+
+
+
+}
+
+func main(){
+	fmt.Println(Index("8eLp47Gz~5{/c", "eLp4"))
 }
